1bbc84c1393f825814e3b65df67748bff02fca8e
tanhancheng <hancheng.tan@lockinchina.com>
2019-09-23 15:20:00 +0800

    commit index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { render } from "react-dom";
 import { TextInput } from "./lib";
+import { H1, H3 } from "./lib/H";
+import Text from "./lib/Text";
+import Button from "./lib/Button";
+import TextInput1 from "./lib/TextInput1";
+import './iconfont/iconfont.css';
 
 const App = () => (
   <div style={{ width: 640, margin: "15px auto" }}>
-    <h1>Demo For Components inside this library</h1>
+    <H1 bold>Demo For Components inside this library</H1>
+    <Text size={16}>Demo For Components inside this library</Text>
+      <Button type={1} hasIcon IconName={"\\e633"}>按钮</Button>
+      <Button type={2}>按钮</Button>
+      <Button type={3}>按钮</Button>
+      <Button type={4} hasIcon IconName={"\\e603"}>按钮</Button>
     <TextInput label="Email Address" placeholder="name@example.com" big={true}/>
+    <TextInput1 label="Email Address" placeholder="name@example.com" big={true} wrongText={''}/>
+    <TextInput1 label="Email Address" placeholder="name@example.com" big={true} wrongText={'WrongTextWrong'}/>
   </div>
 );
 
---
+++ src/lib/Button.js
@@ -0,0 +1,110 @@
+import styled, { css } from 'styled-components';
+import React, { Children } from 'react';
+import ProTypes from 'prop-types';
+
+function ButtonStyle (props){
+    // type 按钮样式4种 hasIcon 是否有Icon
+    const { type, onClick, hasIcon, style, className, IconName } = props;
+    const ButtonBasic = css`
+        font-size: 16px;
+        padding: 0 30px;
+        height: 40px;
+        line-height: 40px;
+        border-radius: 4px;
+        cursor: pointer;
+        position: relative;
+        &:focus{ outline: none; }
+    `;
+
+    let ButtonStyle = styled.button`
+        ${ButtonBasic};
+        ${type === 1 && css`
+            color: #fff;
+            background-color: #000099;
+            &:active {
+              background-color: rgba(0,0,153,1);
+            };
+            &:hover {
+              background-color:rgba(0,78,201,1);
+            };
+        `};
+        ${type === 2 && css`
+            color: #000099;
+            background-color: #fff;
+            border: 1px solid #000099;
+            &:active {
+              border-color: rgba(0,0,153,1);
+            };
+            &:hover {
+              background-color: rgba(236,243,255,1);
+              border-color: rgba(0,0,153,1);
+            };
+        `};
+        ${type === 3 && css`
+            color: #535353;
+            background-color: #fff;
+            border:1px solid rgba(90,90,90,1);
+            &:active {
+              border-color: rgba(0,0,153,1);
+            };
+            &:hover {
+              color: rgba(143,143,143,1);
+              border-color: rgba(143,143,143,1);
+            };
+        `};
+        ${type === 4 && css`
+            color: #000099;
+            background-color: #fff;
+            border:none;
+            &:active {
+              background:rgba(240,246,255,1);
+            };
+            &:hover {
+              background:rgba(246,250,255,1);
+            };
+        `};
+    `;
+
+const IconDiv = styled.div`
+  position: relative;
+  ${hasIcon && css`
+    padding-left: 20px;
+    &:before {
+      font-family: "lockinu" !important;
+      content: '${IconName}';
+      position: absolute;
+      font-size: 12px;
+      top: 0;
+      left: 0px;
+    }
+  `};
+`;
+
+    return (
+        <ButtonStyle type={type} className={className} style={style} hasIcon={hasIcon} onClick={() => onClick()}>
+            <IconDiv>
+                {Children.toArray(props.children)}
+            </IconDiv>
+        </ButtonStyle>
+    );
+}
+
+ButtonStyle.ProTypes = {
+    type: ProTypes.number,
+    onClick: ProTypes.func,
+    hasIcon: ProTypes.bool,
+    style: ProTypes.object,
+    className: ProTypes.string,
+    IconName: ProTypes.string,
+};
+
+ButtonStyle.defaultProps = {
+    type: 1,
+    onClick: () => {},
+    hasIcon: false,
+    style: {},
+    className: '',
+    IconName: '',
+};
+
+export default ButtonStyle;
