4814279e2e76345b6127cd3172b80c1579342ed2
LiNing <ning.li@lockinchina.com>
2019-09-29 16:06:40 +0800

    组件修改优化

--- src/containers/inputDemo/InputDemo.js
+++ src/containers/inputDemo/InputDemo.js
@@ -75,6 +75,7 @@ export default function InputDemo() {
           leftIconColor="#005ED4"
           leftIconSize={15}
           wrongText="我是错误提示！"
+          // inputBackgroundColor="red"
         />
 
         <Space />
@@ -113,6 +114,8 @@ export default function InputDemo() {
           <tr><td>rightIconOnClick</td><td>右侧icon图标点击事件 </td><td>function</td><td>/</td></tr>
           <tr><td>rightIconTitle</td><td>右侧icon图标title(鼠标浮入显示title文字) </td><td>string</td><td>/</td></tr>
           <tr><td>inputBorderColor</td><td>设置文本框边框颜色 </td><td>string</td><td>#a0a0a0</td></tr>
+          <tr><td>inputBackgroundColor</td><td>设置文本框边框背景色 </td><td>string</td><td>#fff</td></tr>
+          
         </tbody>
       </Table>
       <Space />
