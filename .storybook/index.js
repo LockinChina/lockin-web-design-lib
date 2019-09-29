e5592d25072fa2bd6a294ee2fb332cc9a413bbf6
tanhancheng <hancheng.tan@lockinchina.com>
2019-09-29 15:40:49 +0800

    remove button

--- .storybook/index.js
+++ .storybook/index.js
@@ -18,8 +18,8 @@ import InputDemo from '../src/containers/inputDemo/InputDemo';
 import TextAreaDemo from '../src/containers/textAreaDemo/textAreaDemo';
 import CascadeSelectDemo from '../src/containers/cascadeSelectDemo/CascadeSelectDemo';
 import DatePickerDemo from '../src/containers/datePickerDemo/datePickerDemo';
-import SkillSliderDemo from '../src/containers/skillSliderDemo/SkillSliderDemo';
-import Image from '../src/containers/imageDemo/ImageDemo';
+import SkillSliderDemo from '../src/containers/SkillSliderDemo/SkillSliderDemo';
+import Image from '../src/containers/ImageDemo/ImageDemo';
 import Loading from '../src/containers/loadingDemo/LoadingDemo';
 import EmptyDemo from '../src/containers/emptyDemo/EmptyDemo'
 
