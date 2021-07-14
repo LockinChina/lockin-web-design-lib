aa020a603776c5a357c5718a48eb619f312991a5
Ray Huang <rui.huang@lockinchina.com>
2021-07-14 10:12:11 +0800

    version bump

--- dist/dts/stories/Button.stories.d.ts
+++
@@ -1,5 +0,0 @@
-import { Story, Meta } from '@storybook/react';
-declare const _default: Meta<import("@storybook/react").Args>;
-export default _default;
-export declare const BackColor: Story<any>;
-export declare const Small: Story<any>;
