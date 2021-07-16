0c8f76c283565bbac582ddb430dd269166f83361
Ray Huang <rui.huang@lockinchina.com>
2021-07-16 11:34:57 +0800

    version bump

--- rollup.config.js
+++ rollup.config.js
@@ -8,33 +8,33 @@ import dts from 'rollup-plugin-dts';
 
 import packageJson from './package.json';
 
-export default [
-  {
-    input: './src/index.ts',
-    output: [
-      {
-        file: packageJson.main,
-        format: 'cjs',
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript(),
-      postcss(),
-    ],
-  },
-  // {
-  //   // path to your declaration files root
-  //   input: './dist/dts/index.d.ts',
-  //   output: [{file: 'build/index.d.ts', format: 'es'}],
-  //   plugins: [dts()],
-  // },
-];
+export default
+{
+  input: './src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript(),
+    postcss(),
+  ],
+}
+// {
+//   // path to your declaration files root
+//   input: './dist/dts/index.d.ts',
+//   output: [{file: 'build/index.d.ts', format: 'es'}],
+//   plugins: [dts()],
+// },
+;
