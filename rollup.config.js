73dd576dab8245e11804cc917504f4e4870d593f
Ray Huang <rui.huang@lockinchina.com>
2021-07-18 21:52:28 +0800

    version bump

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
 import postcss from 'rollup-plugin-postcss';
 
-import packageJson from './package.json';
+const packageJson = require('./package.json');
 
-// eslint-disable-next-line import/no-anonymous-default-export
 export default {
-  input: './src/index.ts',
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
@@ -25,7 +24,9 @@ export default {
     peerDepsExternal(),
     resolve(),
     commonjs(),
-    typescript(),
-    postcss(),
+    typescript({useTsconfigDeclarationDir: true}),
+    postcss({
+      extensions: ['.css'],
+    }),
   ],
 };
