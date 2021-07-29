85af4397474eda30956de6cdbd28cad450356a78
Ray Huang <rui.huang@lockinchina.com>
2021-07-29 17:01:49 +0800

    merge

---
+++ dist/dts/stories/Button.stories.d.ts
@@ -0,0 +1,6 @@
+import { Story, Meta } from '@storybook/react';
+import { ButtonProps } from '../components/Button';
+declare const _default: Meta<import("@storybook/react").Args>;
+export default _default;
+export declare const BackColor: Story<ButtonProps>;
+export declare const Small: Story<ButtonProps>;
